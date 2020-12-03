935f479d1aa9074cdc9c1cf59663757a7a5b5e66
Ria-verma <riaverma135@gmail.com>
2020-12-03 18:11:04 +0530

    Added Navbar

--- dev_project/frontend/rentit_front/src/App.js
+++ dev_project/frontend/rentit_front/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import NavBar from './containers/Navbar'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
 
 export default App;
+
+/* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header> */
