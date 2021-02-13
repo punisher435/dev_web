32091531dd27f711bc61d8297b5d40cd045e26a7
punisher435 <kotharichirag1989@gmail.com>
2021-02-13 14:57:25 +0530

    removed bug

--- dev_project/backend/rentit/bookings/views.py
+++ dev_project/backend/rentit/bookings/views.py
@@ -517,24 +517,46 @@ class room_booking(viewsets.ViewSet):
                 room.book10=list1[9]
 
             elif temp>=room.capacity:
-                list1.sort()
-                while temp<=10:
-                    list1.append(None)
-                    temp=temp+1
-                room.booked_by=room.capacity
-                room.bookedtill=list1[0]
-                room.booked=True
+                if temp>10:
 
-                room.book1=list1[0]
-                room.book2=list1[1]
-                room.book3=list1[2]
-                room.book4=list1[3]
-                room.book5=list1[4]
-                room.book6=list1[5]
-                room.book7=list1[6]
-                room.book8=list1[7]
-                room.book9=list1[8]
-                room.book10=list1[9]
+                    list1.sort(reverse=True)
+    
+                    room.booked_by=room.capacity
+                    room.bookedtill=list1[9]
+                    room.booked=True
+
+                    room.book1=list1[9]
+                    room.book2=list1[8]
+                    room.book3=list1[7]
+                    room.book4=list1[6]
+                    room.book5=list1[5]
+                    room.book6=list1[4]
+                    room.book7=list1[3]
+                    room.book8=list1[2]
+                    room.book9=list1[1]
+                    room.book10=list1[0]
+
+
+                else:
+
+                    list1.sort()
+                    while temp<=10:
+                        list1.append(None)
+                        temp=temp+1
+                    room.booked_by=room.capacity
+                    room.bookedtill=list1[0]
+                    room.booked=True
+
+                    room.book1=list1[0]
+                    room.book2=list1[1]
+                    room.book3=list1[2]
+                    room.book4=list1[3]
+                    room.book5=list1[4]
+                    room.book6=list1[5]
+                    room.book7=list1[6]
+                    room.book8=list1[7]
+                    room.book9=list1[8]
+                    room.book10=list1[9]
 
             room.save()
 
@@ -770,24 +792,46 @@ class room_booking(viewsets.ViewSet):
                             room.book10=list1[9]
 
                         elif temp>=room.capacity:
-                            list1.sort()
-                            while temp<=10:
-                                list1.append(None)
-                                temp=temp+1
-                            room.booked_by=room.capacity
-                            room.bookedtill=list1[0]
-                            room.booked=True
+                            if temp>10:
 
-                            room.book1=list1[0]
-                            room.book2=list1[1]
-                            room.book3=list1[2]
-                            room.book4=list1[3]
-                            room.book5=list1[4]
-                            room.book6=list1[5]
-                            room.book7=list1[6]
-                            room.book8=list1[7]
-                            room.book9=list1[8]
-                            room.book10=list1[9]
+                                list1.sort(reverse=True)
+                
+                                room.booked_by=room.capacity
+                                room.bookedtill=list1[9]
+                                room.booked=True
+
+                                room.book1=list1[9]
+                                room.book2=list1[8]
+                                room.book3=list1[7]
+                                room.book4=list1[6]
+                                room.book5=list1[5]
+                                room.book6=list1[4]
+                                room.book7=list1[3]
+                                room.book8=list1[2]
+                                room.book9=list1[1]
+                                room.book10=list1[0]
+
+
+                            else:
+
+                                list1.sort()
+                                while temp<=10:
+                                    list1.append(None)
+                                    temp=temp+1
+                                room.booked_by=room.capacity
+                                room.bookedtill=list1[0]
+                                room.booked=True
+
+                                room.book1=list1[0]
+                                room.book2=list1[1]
+                                room.book3=list1[2]
+                                room.book4=list1[3]
+                                room.book5=list1[4]
+                                room.book6=list1[5]
+                                room.book7=list1[6]
+                                room.book8=list1[7]
+                                room.book9=list1[8]
+                                room.book10=list1[9]
 
                         room.save()
 
