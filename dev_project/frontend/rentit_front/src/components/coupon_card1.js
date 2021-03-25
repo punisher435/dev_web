ea16a2e9327f1c181916c58383c425562f2103b7
punisher435 <kotharichirag1989@gmail.com>
2021-03-26 00:14:49 +0530

    new

--- dev_project/frontend/rentit_front/src/components/coupon_card1.js
+++ dev_project/frontend/rentit_front/src/components/coupon_card1.js
@@ -6,10 +6,16 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import {Link} from 'react-router-dom'
+import './css/App.css';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
   root: {
-  
+   
+    width: 300,
+    margin:10,
+    boxShadow: '10px 10px 5px grey',
+    
   },
   bullet: {
     display: 'inline-block',
@@ -17,11 +23,32 @@ const useStyles = makeStyles({
     transform: 'scale(0.8)',
   },
   title: {
-    fontSize: 14,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  title1: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
   },
   pos: {
     marginBottom: 12,
   },
+  coupontext:{
+    color: 'white',
+    padding: '2px 5px 2px 5px',
+  },
+  imgclass:{
+    width: '100%',
+  },
+  imgclass1:{
+    width:'70vw',
+    maxWidth: 80,
+
+  }
+  
 });
 
 export default function CouponCard({mycoupon}) {
@@ -29,42 +56,151 @@ export default function CouponCard({mycoupon}) {
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
+    <div className={classes.root}>
+      <div className="blockcoupon">
+        <div className="couponborder">
+          {
+            mycoupon.coupon_type==='off_price' ? <div className={classes.imgclass1}><img src='/discount_off.png' className={classes.imgclass}/></div> : <div className={classes.imgclass1}><img src='/discount.png' className={classes.imgclass}/></div>
+          }
+
+          
+       
+       
+      <Grid
+  container
+  direction="column"
+  justify="flex-start"
+  alignItems="flex-start"
+  
+>
+ 
+
+  <Grid
+  container
+  direction="row"
+  justify="center"
+  alignItems="center"
+  
+>
+
+        <Typography className={classes.title} color="textSecondary">
+            {
+              mycoupon.coupon_type==='off_price' ? `${mycoupon.currency.slice(0,1)} ${mycoupon.off} off` : `${mycoupon.off} % Discount`
+          }
+        </Typography>
+
+</Grid>
+        
+        <br />
+        <Grid
+  container
+  direction="row"
+  justify="flex-start"
+  alignItems="flex-start"
+  
+>
+  <Grid item xs={2}>
+        <div className="circle2" ></div></Grid>
+
+          <Grid item xs={8}>
+          <Typography variant="body2" className={classes.title1} component="p">
+        Applicable from - {
+              mycoupon.valid_from
+          } </Typography>
+          
+          <Typography variant="body2" className={classes.title1} component="p">
+          on some specific products, 
+          </Typography>
+
+          <Typography variant="body2" className={classes.title1} component="p">
+           
+        Min. price for coupon to be 
+        </Typography>
+        
+        <Typography variant="body2" className={classes.title1} component="p">
+
+        applicable is {mycoupon.currency} {
+              mycoupon.min_price
+          },
+           </Typography>
+          
+         
+         
+          {
+            mycoupon.max_off_price ?  <Typography variant="body2" className={classes.title1} component="p">`Max discount will be ${mycoupon.currency} ${mycoupon.max_off_price},` 
+             </Typography> : null
+          
+          }
+        
+        <Typography variant="body2" className={classes.title1} component="p">
+        Expiry on 
           
           {
-              mycoupon.coupon_type==='off_price' ? 'Price off Coupon' : 'Discount Coupon'
-          }
-        </Typography>
-        <Typography variant="h5" component="h2">
-        {mycoupon.coupoun_code}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          {mycoupon.off} {
-              mycoupon.coupon_type==='off_price' ? 'off in Price' : '% Discount'
-          }
-        </Typography>
-        <Typography variant="body2" component="p">
-        Valid from {
-              mycoupon.valid_from
-          }
-          <br />
-          Valid till {
               mycoupon.expiry_date
           }
-        </Typography>
-        <Typography variant="body2" component="p">
-        Min. price for discount  {
-              mycoupon.min_price
-          }
-          <br />
-         Max_discount {
-              mycoupon.max_off_price
-          }
-        </Typography>
-      </CardContent>
+          </Typography>
       
-    </Card>
+        
+          
+          </Grid>
+
+
+        <Grid item xs={2}>
+        <Grid
+  container
+  direction="row"
+  justify="flex-end"
+  alignItems="flex-start"
+  
+>
+        <div className="circle3"></div></Grid></Grid>
+        </Grid>
+        
+        </Grid>
+        
+
+       
+        
+
+
+        <br />
+
+        <Grid
+  container
+  direction="row"
+  justify="center"
+  alignItems="center"
+  
+  
+>
+  <div className="titlecoupon">
+<Typography variant="h5" component="h2" className={classes.coupontext}>
+        {mycoupon.coupoun_code}
+        </Typography>
+        </div>
+      
+
+</Grid>
+<br />
+
+       
+      
+        
+        
+        
+        
+       
+          
+       
+        
+      
+      
+    
+        </div>
+
+
+
+        </div>
+    </div>
   );
 }
