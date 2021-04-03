0fca2036c36dd608b148e5ffa53d49a0c3040159
punisher435 <kotharichirag1989@gmail.com>
2021-04-03 12:55:50 +0530

    new

--- dev_project/backend/rentit/complaint/urls_room.py
+++ dev_project/backend/rentit/complaint/urls_room.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import room_complaints
+from .views import room_complaint
 
 router = DefaultRouter()
-router.register(r'complaints/room', room_complaints, basename='room_complaints')
+router.register(r'complaints/room', room_complaint, basename='room_complaints')
 urlpatterns = router.urls 
