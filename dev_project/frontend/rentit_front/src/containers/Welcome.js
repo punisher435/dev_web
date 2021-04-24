3dd88d3f08d7530ad6732bbe6e23d847ce59aa9d
punisher435 <kotharichirag1989@gmail.com>
2021-04-24 22:10:57 +0530

    new

--- dev_project/frontend/rentit_front/src/containers/Welcome.js
+++ dev_project/frontend/rentit_front/src/containers/Welcome.js
@@ -7,15 +7,40 @@ import '../components/css/App.css'
 
 const useStyles = makeStyles((theme) => ({
    imgclass:{
-    filter: 'brightness(50%)',
-    height: '80vh',
+    
+    height: 800,
     objectFit:'cover',
   
    
   
    },
+
+   imageclass1: {
+    overflow: 'hidden',
+      width: '140px',
+      height: '140px',
+      position:'relative',
+    borderRadius:'30%',
+    [theme.breakpoints.up('sm')]: {
+      borderRadius:'30%',
+      overflow: 'hidden',
+      width: '170px',
+      height: '170px',
+      position:'relative',
+    },
+    [theme.breakpoints.up('md')]: {
+      borderRadius:'30%',
+      overflow: 'hidden',
+      width: '200px',
+      height: '200px',
+      position:'relative',
+    },
+    marginRight:'1%',
+    marginLeft:'1%',
+  
+  },
    myclass:{
-   height: '80vh',
+   height: 800,
 
    
    
@@ -29,7 +54,7 @@ function Welcome(){
     <div>
         <Carousel className={classes.myclass} controls={false} indicators={false} fade={true}>
             <Carousel.Item>
-            
+                
                 <img
                 className={classes.imgclass}
                 src="https://image.freepik.com/free-photo/living-room-arrangement-with-yoga-mat_23-2148741917.jpg"
@@ -47,7 +72,7 @@ function Welcome(){
                 </Grid>
                 
                 
-                <div className="container_class">
+                <div className="container_class1">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,128L60,112C120,96,240,64,360,58.7C480,53,600,75,720,101.3C840,128,960,160,1080,170.7C1200,181,1320,171,1380,165.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
             </div>
           
